47dd5b9aa6589eaad04d6398de666ed98b91d276
Philippe Corrèges <philippe.correges@io-software.fr>
2018-07-25 12:02:16 +0200

    1st Commit

---
+++ server/config.local.js
@@ -0,0 +1,10 @@
+/**
+ * Created by IO Software on 25/07/2018.
+ */
+var p = require('../package.json');
+var version = p.version.split('.').shift();
+module.exports = {
+  restApiRoot: '/api' + (version > 0 ? '/v' + version : ''),
+  host: process.env.HOST || 'localhost',
+  port: process.env.PORT || 3000
+};
