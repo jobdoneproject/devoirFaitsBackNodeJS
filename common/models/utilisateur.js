4f1dfeb20efc3befd9b5c37a4e88a5d9f41bd8d1
Philippe Corrèges <philippe.correges@io-software.fr>
2018-08-03 18:12:47 +0200

    Created new models without ACL

--- common/models/utilisateur.js
+++ common/models/utilisateur.js
@@ -1,5 +1,11 @@
 'use strict';
-
 module.exports = function(Utilisateur) {
+  var app;
+  Utilisateur.on('attached',function(a)
+      {
+        app = a;
+          console.log('utilisateur connecté');
 
+
+        });
 };
