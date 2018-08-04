fda8d78ec310339c013cee8a439457e48cf66a52
Philippe Corrèges <philippe.correges@io-software.fr>
2018-08-04 19:25:15 +0200

    A non yet working remote method for tracing Application Access

--- common/models/utilisateur.js
+++ common/models/utilisateur.js
@@ -1,11 +1,12 @@
 'use strict';
-module.exports = function(Utilisateur) {
-  var app;
-  Utilisateur.on('attached',function(a)
-      {
-        app = a;
-          console.log('utilisateur connecté');
-
-
-        });
+var app = require('../../server/server');
+module.exports = function (Utilisateur) {
+  /*
+  Utilisateur.afterRemote('login', function(ctx, next) {
+    var userlog=app.models.userlog;
+    userlog.create({userFirstName: 'foo@bar.com',userSirName: 'foo@bar.com', userEmail: 'foo@bar.com', logindatetime: Date(), actionType: 'login'},  function(err, userInstance) {
+      console.log(err,userInstance);
+    });
+     });
+     */
 };
