3eb122093b9ecf5d9d1927c2585493c1d4e8edce
Philippe Corrèges <philippe.correges@io-software.fr>
2018-08-22 11:05:56 +0200

    Changed ID properties

--- common/models/utilisateur.js
+++ common/models/utilisateur.js
@@ -1,12 +1,13 @@
 'use strict';
 var app = require('../../server/server');
 module.exports = function (Utilisateur) {
-  /*
-  Utilisateur.afterRemote('login', function(ctx, next) {
+/*
+  Utilisateur.afterRemote('login', async function(ctx, user, next) {
     var userlog=app.models.userlog;
-    userlog.create({userFirstName: 'foo@bar.com',userSirName: 'foo@bar.com', userEmail: 'foo@bar.com', logindatetime: Date(), actionType: 'login'},  function(err, userInstance) {
-      console.log(err,userInstance);
-    });
-     });
-     */
+    userlog.create({user.nom,'foo@bar.com', 'foo@bar.com', Date(), 'login'},  function(err, userInstance) {
+      console.log(err,user);
+
+      });
+
+     });*/
 };
